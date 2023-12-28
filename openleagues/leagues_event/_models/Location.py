ed0bce6544f1cdaf6b1048e8f5ba8e5b2a55803d
taher-dohadwala <taher.dohadwala1@gmail.com>
2023-12-28 17:14:31 -0600

    updating Location fields with city instead of county

--- openleagues/leagues_event/_models/Location.py
+++ openleagues/leagues_event/_models/Location.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Location(models.Model):
     state = models.CharField(max_length=50)
-    county = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
     zipcode = models.CharField(max_length=10)
 
     def __str__(self):
-        return f"{self.county}, {self.state} ({self.zipcode})"
+        return f"{self.city}, {self.state} ({self.zipcode})"
