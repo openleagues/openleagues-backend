ea2d402937312a4d51b44cb126fdfdcaf70ae5ba
taher-dohadwala <taher.dohadwala1@gmail.com>
2023-12-30 01:36:29 -0600

    add view for details, update, and delete

--- api/settings/base.py
+++ api/settings/base.py
@@ -60,7 +60,7 @@ SIMPLE_JWT = {
         "Bearer",
         "JWT"
     ),
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "SIGNING_KEY": env("SIGNING_KEY"),
     "AUTH_HEADER_NAME": "HTTP_AUTHENTICATION",
--- openleagues/leagues_event/views.py
+++ openleagues/leagues_event/views.py
@@ -1,13 +1,16 @@
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from openleagues.leagues_event.models import LeaguesEvent
 from openleagues.leagues_event.serializer import LeaguesEventSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from openleagues.leagues_event.permissions import IsOwnerOrReadOnly
 
+class LeaguesEventList(generics.ListCreateAPIView):
+    queryset = LeaguesEvent.objects.all()
+    serializer_class = LeaguesEventSerializer
 
-@csrf_exempt
-def leagues_event_list(request):
-    
-    if request.method == "GET":
-        events = LeaguesEvent.objects.all()
-        serializer = LeaguesEventSerializer(events, many=True)
-        return JsonResponse(serializer.data, safe=False)
+class LeaguesEventDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = LeaguesEvent.objects.all()
+    # Change serializer for more information
+    serializer_class = LeaguesEventSerializer
+    lookup_field = "id"
+    #permission_classes = [IsAuthenticated]
