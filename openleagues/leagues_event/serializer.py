6b93a8575196bc32b02570bf22acfe9afa788952
taher-dohadwala <taher.dohadwala1@gmail.com>
2023-12-29 22:49:03 -0600

    refactor LeaguesEventSerializer to include location

--- openleagues/leagues_event/serializer.py
+++ openleagues/leagues_event/serializer.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from openleagues.leagues_event.models import LeaguesEvent
+from openleagues.leagues_event.models import LeaguesEvent, Location
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        exclude = ["id"]
 
 class LeaguesEventSerializer(serializers.ModelSerializer):
+    location = LocationSerializer(read_only=True)
     class Meta:
         model = LeaguesEvent
         exclude = ["pkid", "updated_at"]
