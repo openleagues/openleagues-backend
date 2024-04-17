8b71fa0feb41cae7ab668baf7158f87f45459bbf
taher-dohadwala <taher.dohadwala1@gmail.com>
2024-04-17 17:30:56 -0500

    remove junk from settings

--- api/settings/base.py
+++ api/settings/base.py
@@ -67,24 +67,6 @@ SIMPLE_JWT = {
     "USER_ID_FIELD": "id"
 }
 
-# DJOSER = {
-#     "LOGIN_FIELD": "email",
-#     "USER_CREATE_PASSWORD_RETYPE": True,
-#     "USERNAME_CHANGED_EMAIL_CONFIRMATION": True,
-#     "SEND_CONFIRMATION_EMAIL": True,
-#     "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
-#     "SET_PASSWORD_RETYPE": True,
-#     "PASSWORD_RESET_CONFIRM_RETYPE": True,
-#     "USERNAME_RESET_CONFIRM_URL": "email/reset/confirm/{uid}/{token}",
-#     "ACTIVATION_URL": "activate/{uid}/{token}",
-#     "SEND_ACTIVATION_EMAIL": True,
-#     "SERIALIZERS": {
-#         "user_create": "",
-#         "user": "",
-#         "current_user": "",
-#         "user_delete": ""
-#     }
-# }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
